ec66488a5f9c002d8c5ccdb7d7d33ddd51f1aed0
a_2006 <156690663+a2006-dev@users.noreply.github.com>
2026-07-06 15:40:27 +0800

    Rename app/build.gradle to app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.logcatcher"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.logcatcher"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            minifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    // Shizuku API (本地 AAR，避免远程下载问题)
+    implementation(files("libs/shizuku-api-13.1.5.aar"))
+    implementation(files("libs/shizuku-provider-13.1.5.aar"))
+    implementation(files("libs/shizuku-aidl-13.1.5.aar"))
+}
